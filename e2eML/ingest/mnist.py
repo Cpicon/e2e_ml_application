9496107f17f6b48ec988c941ee7c95e9206a4e7b
ChristianP <c.picon@uniandes.edu.co>
2024-08-16 18:55:03 -0500

    Refactor MNIST ingestion and add DataLoader functionality
    Refactor the MNIST ingestion functions to separate configuration paths and filenames, improve documentation, and ensure directories exist before saving data. Introduce a new `get_data_loader` function to create training and test DataLoaders, and add the `print_data` function to visualize a batch of training data.

--- e2eML/ingest/mnist.py
+++ e2eML/ingest/mnist.py
@@ -1,28 +1,45 @@
 import base64
-import json
+import os
 from io import BytesIO
-
-import requests
-import pandas as pd
 import torch
 from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 from dagster import (
     MaterializeResult,
     MetadataValue,
-    asset, AssetExecutionContext, DataVersion, Config
+    asset,
+    AssetExecutionContext,
+    Config,
+    AssetOut,
+    multi_asset,
 )
 
 
 class MNISTConfig(Config):
-    training_save_path: str = "data/MNIST/train/mnist_training_data.pt"
-    test_save_path: str = "data/MNIST/test/mnist_test_data.pt"
+    train_folder_path: str = "data/MNIST/train/"
+    test_folder_path: str = "data/MNIST/test/"
+    train_filename: str = "mnist_train_data.pt"
+    test_filename: str = "mnist_test_data.pt"
+    batch_size: int = 64
 
 
 @asset
-def train_MNIST_data(context: AssetExecutionContext, config : MNISTConfig) -> MaterializeResult:
+def train_MNIST_data(
+    context: AssetExecutionContext, config: MNISTConfig
+) -> MaterializeResult:
+    """Downloads, processes, and saves the MNIST training data.
+
+    Args:
+        context (AssetExecutionContext): The execution context provided by Dagster.
+        config (MNISTConfig): Configuration settings including paths and filenames.
+
+    Returns:
+        MaterializeResult: The result of materializing the training data, including metadata such as
+            the number of records, a preview of the data, and the label mappings.
+    """
     training_data = datasets.MNIST(
         root="data",
         train=True,
@@ -32,15 +49,16 @@ def train_MNIST_data(context: AssetExecutionContext, config : MNISTConfig) -> Ma
 
     context.log.info(f"Training data: {training_data}")
     context.log.info(f"Raw data save to: {training_data.raw_folder}")
-    torch.save(training_data, config.training_save_path)
-    context.log.info(f"Data saved to {config.training_save_path}")
+    os.makedirs(config.train_folder_path, exist_ok=True)
+    save_path = os.path.join(config.train_folder_path, config.train_filename)
+    torch.save(training_data, save_path)
+    context.log.info(f"Data saved to {save_path}")
 
-    # Make a bar chart of the top 25 words
     img, target = training_data[0]
     plt.figure(figsize=(10, 6))
     plt.title(f"Label: {target}")
-    plt.imshow(img[0], cmap='gray')
-    plt.axis('off')
+    plt.imshow(img[0], cmap="gray")
+    plt.axis("off")
     plt.tight_layout()
 
     # Convert the image to a saveable format
@@ -62,25 +80,38 @@ def train_MNIST_data(context: AssetExecutionContext, config : MNISTConfig) -> Ma
 
 
 @asset
-def test_MNIST_data(context: AssetExecutionContext, config: MNISTConfig) -> MaterializeResult:
+def test_MNIST_data(
+    context: AssetExecutionContext, config: MNISTConfig
+) -> MaterializeResult:
+    """Downloads, processes, and saves the MNIST test data.
+
+    Args:
+        context (AssetExecutionContext): The execution context provided by Dagster.
+        config (MNISTConfig): Configuration settings including paths and filenames.
+
+    Returns:
+        MaterializeResult: The result of materializing the test data, including metadata such as
+            the number of records, a preview of the data, and the label mappings.
+    """
     test_data = datasets.MNIST(
         root="data",
         train=False,
         download=True,
         transform=ToTensor(),
     )
-
     context.log.info(f"Training data: {test_data}")
     context.log.info(f"Raw data save to {test_data.raw_folder}")
-    torch.save(test_data, config.test_save_path)
-    context.log.info(f"Data saved to {config.test_save_path}")
 
-    # Make a bar chart of the top 25 words
+    os.makedirs(config.test_folder_path, exist_ok=True)
+    save_path = os.path.join(config.test_folder_path, config.test_filename)
+    torch.save(test_data, save_path)
+    context.log.info(f"Data saved to {save_path}")
+
     img, target = test_data[0]
     plt.figure(figsize=(10, 6))
     plt.title(f"Label: {target}")
-    plt.imshow(img[0], cmap='gray')
-    plt.axis('off')
+    plt.imshow(img[0], cmap="gray")
+    plt.axis("off")
     plt.tight_layout()
 
     # Convert the image to a saveable format
@@ -101,4 +132,72 @@ def test_MNIST_data(context: AssetExecutionContext, config: MNISTConfig) -> Mate
     )
 
 
+@multi_asset(
+    outs={
+        "test_dataloader": AssetOut(),
+        "train_dataloader": AssetOut(),
+    },
+    deps=[train_MNIST_data, test_MNIST_data],
+)
+def get_data_loader(context: AssetExecutionContext, config: MNISTConfig):
+    """Loads the MNIST training and test data and returns DataLoaders.
 
+    Args:
+        context (AssetExecutionContext): The execution context provided by Dagster.
+        config (MNISTConfig): Configuration settings including paths, filenames, and batch size.
+
+    Returns:
+        tuple: A tuple containing the test and train DataLoader objects.
+    """
+    training_path = os.path.join(config.train_folder_path, config.train_filename)
+    training_data = torch.load(training_path)
+    context.log.info(f"Training data loaded: {training_data}")
+    train_dataloader = DataLoader(training_data, batch_size=config.batch_size)
+
+    test_path = os.path.join(config.test_folder_path, config.test_filename)
+    test_data = torch.load(test_path)
+    context.log.info(f"Test data loaded: {test_data}")
+    test_dataloader = DataLoader(test_data, batch_size=config.batch_size)
+    return test_dataloader, train_dataloader
+
+
+@asset
+def print_data(train_dataloader) -> MaterializeResult:
+    """Generates and logs a visual preview of the first batch of MNIST training data.
+
+    Args:
+        train_dataloader (DataLoader): The DataLoader for the MNIST training data.
+
+    Returns:
+        MaterializeResult: The result of materializing the data preview, including metadata
+            such as the shape and dtype of the data, and a visual preview of the images.
+    """
+    image_data = None
+    for X, y in train_dataloader:
+        # Create a figure with subplots arranged in a grid
+        fig, axes = plt.subplots(2, 5, figsize=(15, 6))  # 2 rows, 5 columns
+
+        for i in range(10):
+            ax = axes[i // 5, i % 5]  # Determine the position in the grid
+            ax.imshow(X[i][0], cmap="gray")  # Display the image
+            ax.set_title(f"Label: {y[i].item()}")  # Set the title as the label
+            ax.axis("off")  # Hide the axes
+
+        plt.tight_layout()  # Adjust subplots to fit in the figure area
+
+        # Convert the image to a saveable format
+        buffer = BytesIO()
+        plt.savefig(buffer, format="png")
+        image_data = base64.b64encode(buffer.getvalue())
+        break
+
+    if image_data is not None:
+        md_content = f"![img](data:image/png;base64,{image_data.decode()})"
+    else:
+        md_content = "No data to display"
+
+    return MaterializeResult(
+        metadata={
+            "preview": MetadataValue.md(md_content),
+        },
+    )
